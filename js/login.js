aa130fb7907949c1511c12be46eecc0ce4da0b6d
lucasal07 <lucasproal22@gmail.com>
2022-08-20 21:33:47 -0300

    Acceso con google

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 const pError = document.getElementById("pError");
 
 function showAlert() {
-    pError.innerHTML =  ` <divrole="alert">
+    pError.innerHTML =  ` <div role="alert">
     <p>Alguno de los campos está vacío, porfavor completalo</p>
     </div> `
 }
@@ -27,6 +27,26 @@ ButtonIngr.addEventListener("click",function(){
     TestCampos();
 })
 
+
+function onSignIn(googleUser) {
+    // Useful data for your client-side scripts:
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    console.log('Full Name: ' + profile.getName());
+    console.log('Given Name: ' + profile.getGivenName());
+    console.log('Family Name: ' + profile.getFamilyName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail());
+    // The ID token you need to pass to your backend:
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log("ID Token: " + id_token);
+  }
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function(){
+      console.log("User signed out.");
+    });
+  }
 
 // function Validar_Campos() {
 
