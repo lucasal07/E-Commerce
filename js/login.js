47f9f6d42f99015e21defe49187deb5cea4780ae
lucasal07 <lucasproal22@gmail.com>
2022-08-20 21:39:48 -0300

    Modificaciones

--- js/login.js
+++ js/login.js
@@ -40,11 +40,14 @@ function onSignIn(googleUser) {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
+    location.href = "main.html";
   }
+
   function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function(){
       console.log("User signed out.");
+      location.href ="index.html";
     });
   }
 
